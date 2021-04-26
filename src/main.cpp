2d24809e1b03fd95a18ca9357c2e17cdee11d0af
stipe <stipe@DESKTOP-QJO2GJ2>
2021-04-26 21:05:31 +0200

    s

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,46 @@
 #include <Arduino.h>
+#include <tmc_vector_control.h>
 
-
-
-// Kontroliranje gimbala za diplomski ver1.0
+vector_control konstrct;
 
 
 int stepPin = 4;
 int dirPin = 5;
 
 
+
 void setup() {
+  /*
   for(int i = 3; i<11; i++)
 {
   pinMode(i,OUTPUT); 
 }
-digitalWrite(9,HIGH);
-digitalWrite(10,HIGH);
+digitalWrite(9,HIGH);   //MS 1
+digitalWrite(10,HIGH); //MS 2
 digitalWrite(8,LOW);
-
-
-
+digitalWrite(stepPin,LOW); 
+  
+    digitalWrite(dirPin,LOW); 
+delay(10);
+*/
+ //dodatak
+ konstrct.SetupZaBrojac();
 }
 
 void loop() {
 
 
-  digitalWrite(dirPin,HIGH); 
-
-  for(int x = 0; x < 5000; x++) {
-    digitalWrite(stepPin,HIGH); 
-    delayMicroseconds(100); 
-    digitalWrite(stepPin,LOW); 
-    delayMicroseconds(100); 
-
-  }
-  delay(1000); 
-  
-
-  digitalWrite(dirPin,LOW); 
-  for(int x = 0; x < 5000; x++) {
+delayMicroseconds(2000);
     digitalWrite(stepPin,HIGH);
-    delayMicroseconds(100);
-    digitalWrite(stepPin,LOW);
-    delayMicroseconds(100);
-  }
-  delay(1000);
+delayMicroseconds(10);
+     digitalWrite(stepPin,LOW);
+
+  
+} 
 
 
+ISR(TIMER1_COMPA_vect)    //rutina koja poziva timer da pozove funkcije izmedju viticastih zagrada svako onoliko vremena koji je određen prescalerima      
+                          // timer compare interrupt service routine
+{
+konstrct.tick();
 }
---
+++ src/tmc_vector_control.cpp
@@ -0,0 +1,42 @@
+#include "tmc_vector_control.h"
+int a = 0;
+int timer = 0;
+//////////////////////////////////////
+vector_control::vector_control(){
+}
+vector_control::~vector_control(){
+}
+void vector_control::SetupZaBrojac(){     
+
+  for(int i = 3; i<11; i++)  // postavljanje pinova 3-10 kao output
+{
+  pinMode(i,OUTPUT); 
+  delay(5);
+  digitalWrite(i,LOW);
+}
+
+digitalWrite(9,HIGH);   //MS 1 pin
+digitalWrite(10,HIGH); //MS 2  pin
+digitalWrite(8,LOW);   // EN pin, (low) za aktivirat driver (HIGH) za deaktivirat driver
+
+
+delay(10);
+
+
+
+  noInterrupts();
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1  = 0;
+  OCR1A = 15624;            // compare match register 16MHz/(256/2Hz)
+  TCCR1B |= (1 << WGM12);   // CTC mode
+  TCCR1B |= ((1 << CS12) | (1 << CS10));  //1024 prescaler
+  TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
+  interrupts();             // enable all interrupts
+}
+void vector_control::klik(){
+
+}
+void vector_control::tick(){
+
+}
