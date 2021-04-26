ec1f1136ac034b96dd062567758c88990ffdf850
stipe <stipe@DESKTOP-QJO2GJ2>
2021-04-26 21:00:29 +0200

    dodana biblioteka 2HZ takt

---
+++ src/tmc_vector_control.h
@@ -0,0 +1,12 @@
+#ifndef tmc_vector_control.h
+#define tmc_vector_control.h
+#include <Arduino.h>
+class vector_control {
+public:
+vector_control();
+~vector_control();
+void SetupZaBrojac();
+void klik();
+void tick();
+};
+#endif
