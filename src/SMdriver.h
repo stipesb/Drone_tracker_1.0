8f3303da9278da3506786ec3dd501b78d473dc99
stipe <stipe@DESKTOP-QJO2GJ2>
2021-04-30 20:39:05 +0200

    preimenovanje_biblioteke

---
+++ src/SMdriver.h
@@ -0,0 +1,15 @@
+#ifndef SMdriver_h
+#define SMdriver_h
+
+#include <Arduino.h>
+class vector_control {
+public:
+vector_control();
+~vector_control();
+void SetupZaBrojac();
+void klik();
+void tick();
+void test();
+};
+#endif
+
